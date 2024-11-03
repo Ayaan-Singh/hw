bc31be8a252b0c86536df0ea8d72a36d50dd6b64
Ayaan-Singh <asthana.divya@gmail.com>
2024-11-03 12:15:48 +0530

    hi

---
+++ lesson3.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a non-negative number: "))
+
+if number >= 0:
+    sqrt = number ** 0.5 
+    print(sqrt)
+else:
+    print("Error: Square root of negative numbers is undefined.")
